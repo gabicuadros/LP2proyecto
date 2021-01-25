bd46abc8346f3745045810ce809b05ee68fc00cd
Enrique Steeven Yugcha Chichanda <esyugcha@espol.edu.ec>
2021-01-25 01:51:07 -0500

    Visualizacion de la tabla labor social

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include("header.php");
+?>
+    <h1>LABOR SOCIAL</h1>
+    <ul>
+        <li><a href="registro.php">Registrar Labor Social</a></li>
+        <li><a href="laborsocial.php">Visualizar Labores Sociales</a></li>
+    </ul>
+<?php
+    include("footer.php");
+?>
